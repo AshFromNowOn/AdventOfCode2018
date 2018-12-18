166594561af7e780da909910a61d3b66954de536
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-12-18 12:30:05 +0000

    Completed day 1

--- day1.py
+++ day1.py
@@ -1,1 +1,29 @@
-with open("sourceFiles/day1Input.txt")
+import sys
+
+def getRepeatedFreq():
+    current_freq = 0
+    freq_list = []
+    repeat_found = False
+    while not repeat_found:
+        with open("sourceFiles/day1input.txt") as f:
+            for line in f:
+                current_freq += int(line)
+                if current_freq in freq_list:
+                    repeat_found = True
+                    break
+                else:
+                    print("Adding {} to list".format(current_freq))
+                    freq_list.append(current_freq)
+    return current_freq
+
+with open("sourceFiles/day1input.txt") as f:
+
+    freq = 0
+    for line in f:
+        freq += int(line)
+    
+print("Final Frequency: {}".format(freq))
+
+
+print("First Repeated Frequency: {}".format(getRepeatedFreq()))
+    
